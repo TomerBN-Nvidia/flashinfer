fe9df7c16a2e9833092bce088d7c8e841fc8e6b5
tbarnatan <tbarnatan@nvidia.com>
2026-07-29 14:04:00 +0300

    Point Super RL FMHA at checksum-valid f8ce artifact

--- flashinfer/artifacts.py
+++ flashinfer/artifacts.py
@@ -135,7 +135,7 @@ class ArtifactPath:
     When compiling new cubins for backend directories, update the corresponding path.
     """
 
-    TRTLLM_GEN_FMHA: str = "158f6fa11ef139a098cfddcdddce73ca99d164ad/fmha/trtllm-gen/"
+    TRTLLM_GEN_FMHA: str = "5a39c4eba9d34e677e328fbd72a02e7e835fc020/fmha/trtllm-gen/"
     TRTLLM_GEN_BMM: str = (
         "d2c5915bf45ae64308a947824c14966c2718a292/batched_gemm-91e0ba0-896b90b/"
     )
@@ -157,7 +157,7 @@ class CheckSumHash:
     """
 
     TRTLLM_GEN_FMHA: str = (
-        "c2d9399b2537be785882354a4f9902ed6c03136c0ea341e201eac40c3923e1dc"
+        "5a39c4eba9d34e677e328fbd72a02e7e835fc0208c51ef6d438e8b1d43e6daa5"
     )
     TRTLLM_GEN_BMM: str = (
         "ffc4f52df6ff901000ab4cd80fa1ae24dcf5fe5739c6e5c0b3bb9b8e4802084e"
